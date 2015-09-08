330804f32788c0d00bfa51843c0d27b4fa738d7d
Ronny Ancorini <Ronnygx@gmail.com>
2015-09-08 13:55:36 +0100

    green: get_an_empty_list_when_logged_user_is_not_friend_of_passed_user

--- TripServiceKata/TripServiceKata.Tests/TripServiceShould.cs
+++ TripServiceKata/TripServiceKata.Tests/TripServiceShould.cs
@@ -2,6 +2,7 @@
 using NUnit.Framework;
 using TripServiceKata.Exception;
 using TripServiceKata.Trip;
+using TripServiceKata.User;
 
 namespace TripServiceKata.Tests
 {
@@ -12,15 +13,31 @@ namespace TripServiceKata.Tests
         [ExpectedException(typeof(UserNotLoggedInException))]
         public static void throw_an_exception_when_no_logged_user()
         {
-            var service = new TestableTripService();
+            var service = new TestableTripService(null);
             service.GetTripsByUser(new User.User());
+        }
+
+        [Test]
+        public static void get_an_empty_list_when_logged_user_is_not_friend_of_passed_user()
+        {
+            var loggedUser = new User.User();
+            var service = new TestableTripService(loggedUser);
+            var trips = service.GetTripsByUser(new User.User());
+            trips.Should().BeEmpty();
         }
 
         private class TestableTripService : TripService
         {
+            private User.User loggedUser;
+
+            public TestableTripService(User.User loggedUser)
+            {
+                this.loggedUser = loggedUser;
+            }
+
             protected override User.User GetLoggedUser()
             {
-                return null;
+                return loggedUser;
             }
         }
     }
