b21586861546d67a7c4ba8a3623cadf4f60ff9c0
Ronny Ancorini <Ronnygx@gmail.com>
2015-09-08 13:23:40 +0100

    green: throw_an_exception_when_no_logged_user

--- TripServiceKata/TripServiceKata.Tests/TripServiceShould.cs
+++ TripServiceKata/TripServiceKata.Tests/TripServiceShould.cs
@@ -1,11 +1,27 @@
-﻿using NUnit.Framework;
+﻿using FluentAssertions;
+using NUnit.Framework;
+using TripServiceKata.Exception;
+using TripServiceKata.Trip;
 
 namespace TripServiceKata.Tests
 {
     [TestFixture]
-    public class TripServiceTest
+    public class TripServiceShould
     {
         [Test]
-        public void 
+        [ExpectedException(typeof(UserNotLoggedInException))]
+        public static void throw_an_exception_when_no_logged_user()
+        {
+            var service = new TestableTripService();
+            service.GetTripsByUser(new User.User());
+        }
+
+        private class TestableTripService : TripService
+        {
+            protected override User.User GetLoggedUser()
+            {
+                return null;
+            }
+        }
     }
 }
